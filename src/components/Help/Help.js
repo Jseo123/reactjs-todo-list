174a49f5a3ca134a4b76074b73f594575e8422c8
Luis Molina <luismateo2416@gmail.com>
2022-02-21 14:17:00 +0100

    markups tutorial

--- src/components/Help/Help.js
+++ src/components/Help/Help.js
@@ -38,29 +38,51 @@ function ModalWindow({ handleModal }) {
         return (
             <>
                 <h1>👋</h1>
-                <p className="getStarted">GET STARTED2</p>
-                <h3>Welcome to the todo list!</h3>
-                <p>Hi, we will help you to understand how the app works!</p>
+                <h3>Creating the first todo!</h3>
+                <p>
+                    you have to write down the tasks that you are going to perform in the future
+                </p>
+                <p>After that, press enter to submit the task</p>
             </>
+        )
+    }
+    const markupInputTask = () => {
+        return (
+
+            <div className="markupInput" />
         )
     }
     const modalThirdSection = () => {
         return (
             <>
                 <h1>👋</h1>
-                <p className="getStarted">GET STARTED3</p>
-                <h3>Welcome to the todo list!</h3>
-                <p>Hi, we will help you to understand how the app works!</p>
+                <h3>Manage your task list!</h3>
+                <p>In this section you can manage the task list as you want, the app can filter your task by done tasks or active tasks,
+                    You can cross out the tasks when you had done it!, or can uncross if you missclicked,
+                </p>
+                <p> The tasks can be eliminated too</p>
+                <p>And if you want to edit the task, press the pencil icon, edit your task, and press Enter to submit!</p>
             </>
         )
 
+    }
+    const markupTaskList = () => {
+        return <div className="markupTaskList" />
     }
     const [modalSection, setModalSection] = useState({
         section: [modalFirstSection, modalSecondSection, modalThirdSection],
         position: 0
     });
-    const handleContinue = () => {
+
+    const handleContinue = (index = null) => {
         const { position } = modalSection
+        if (index === Number) {
+            setModalSection({
+                ...modalSection,
+                position: index
+            })
+            return;
+        }
         setModalSection({
             ...modalSection,
             position: position + 1
@@ -69,11 +91,18 @@ function ModalWindow({ handleModal }) {
     const { position, section } = modalSection
     return (
         <>
-            <div className="modalBackground" />
+            <div className="modalBackground">
+                {position === 1 &&
+                    markupInputTask()
+                }
+                {position === 2 &&
+                    markupTaskList()
+                }
+            </div>
             <section className="modalWindow">
                 <Button className="btnWithIcon" handleClick={handleModal}><img className="deleteIcon" src={cancelIcon} alt="cancel icon" /></Button>
                 {section[position]()}
-                {position <= 1 && <Button handleClick={handleContinue}>Next page</Button>}
+                {position <= 1 && <Button className="nextSectionBtn" handleClick={handleContinue}>Next page</Button>}
             </section>
         </>
     )
